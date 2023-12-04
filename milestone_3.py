e7bde93c5f1f7c1ae0c2e10de714a6aa847cacdf
S-a-a-h <sam_jb_2@hotmail.co.uk>
2023-12-04 15:03:10 +0000

    creates functions for a guess input and validates the guess

--- milestone_3.py
+++ milestone_3.py
@@ -5,16 +5,6 @@ word_list = ['orange', 'kiwi', 'durian', 'rambutan', 'lime']
 word = random.choice(word_list)
 
 
-#asks user for input 
-def ask_for_input():
-    while True:
-        guess = input('Enter a single letter: ')
-        if len(guess) == 1 and guess.isalpha():
-            break
-        else: 
-            print('Invalid letter. Please, enter a single alphabetical character.')
-        
-
 #checks if the guessed letter is in the word
 def check_guess(guess):
     guess.lower()
@@ -22,6 +12,17 @@ def check_guess(guess):
         print(f'Good guess! {guess} is in the word.')
     else:
         print(f'Sorry, {guess} is not in the word. Try again.')
+
+
+#asks user for input 
+def ask_for_input():
+    while True:
+        guess = input('Enter a single letter: ')
+        if len(guess) == 1 and guess.isalpha():
+            print(guess)
+            break
+        else: 
+            print('Invalid letter. Please, enter a single alphabetical character.')
 
 
 ask_for_input()
