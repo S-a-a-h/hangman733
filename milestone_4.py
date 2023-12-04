39a849813cc75e74d4ce709299a296030d72353d
S-a-a-h <sam_jb_2@hotmail.co.uk>
2023-12-04 18:12:33 +0000

    creates Hangman class with 2 methods

---
+++ milestone_4.py
@@ -0,0 +1,67 @@
+import random 
+
+#creates list of words from which a random is selected by computer 
+word_list = ['orange', 'kiwi', 'durian', 'rambutan', 'lime']
+word = random.choice(word_list)
+
+#ask_for_input function
+def ask_for_input():
+    while True:
+        guess = input('Enter a single letter: ')
+        if len(guess) == 1 and guess.isalpha():
+            print(guess)
+            break
+        else: 
+            print('Invalid letter. Please, enter a single alphabetical character.')
+    check_guess(guess)
+
+#check_guess function
+def check_guess(self, guess):
+    guess.lower()
+    if guess in word:
+        print(f'Good guess! {guess} is in the word.')
+    else:
+        print(f'Sorry, {guess} is not in the word. Try again.')
+
+
+#milestone_4.py: class creation
+word_list = ['orange', 'kiwi', 'durian', 'rambutan', 'lime']
+word = random.choice(word_list)
+list_of_guesses = []
+
+class Hangman: 
+    def __init__(self, word_list, num_lives=5):
+        self.word = word
+        self.word_guessed = ['_' for _ in self.word]
+        self.num_letters = len(set(self.word))
+        self.num_lives = num_lives
+        self.word_list = word_list
+        self.list_of_guesses = list_of_guesses
+
+    def check_guess(self, guess): #check_guess method
+        guess = guess.lower()
+        if guess in word:
+            print(f'Good guess! {guess} is in the word.')
+        else:
+            print(f'Sorry, {guess} is not in the word. Try again.')
+
+    def ask_for_input(self): #ask_for_input method
+        while True:
+            guess = input('Enter a single letter: ')
+            if not (len(guess) == 1 and guess.isalpha()):
+                print('Invalid letter. Please, enter a single alphabetical character.')    
+            elif guess in self.list_of_guesses:
+                print('You already tried that letter!')
+            else:
+                self.check_guess(guess)
+                self.list_of_guesses.append(guess)
+                break
+                
+
+hangman = Hangman(['orange', 'kiwi', 'durian', 'rambutan', 'lime'])
+hangman.ask_for_input()
+
+
+
+
+
