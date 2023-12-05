9bb419f28e4622d81e7811d4ef948c637242da31
S-a-a-h <sam_jb_2@hotmail.co.uk>
2023-12-05 11:54:01 +0000

    creates more methods for code clarity and reusability: check_guess, update_word_guessed, lives_left and ask_for_input which calls check_guess, which calls lives_left

--- milestone_4.py
+++ milestone_4.py
@@ -1,30 +1,5 @@
 import random 
 
-#creates list of words from which a random is selected by computer 
-word_list = ['orange', 'kiwi', 'durian', 'rambutan', 'lime']
-word = random.choice(word_list)
-
-#ask_for_input function
-def ask_for_input():
-    while True:
-        guess = input('Enter a single letter: ')
-        if len(guess) == 1 and guess.isalpha():
-            print(guess)
-            break
-        else: 
-            print('Invalid letter. Please, enter a single alphabetical character.')
-    check_guess(guess)
-
-#check_guess function
-def check_guess(self, guess):
-    guess.lower()
-    if guess in word:
-        print(f'Good guess! {guess} is in the word.')
-    else:
-        print(f'Sorry, {guess} is not in the word. Try again.')
-
-
-#milestone_4.py: class creation
 
 #Hangman Class creation
 word_list = ['orange', 'kiwi', 'durian', 'rambutan', 'lime']
